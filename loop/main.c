03f713709f1450b6a81a8da7ac3b845e18e4c4e8
mbruyant <mbruyant@student.42.fr>
2023-12-28 18:10:57 +0100

    gere exit avec un return, faire une fonction separee, ne leak pas, ajoute un commentaire sur une condition de leak

--- loop/main.c
+++ loop/main.c
@@ -6,15 +6,15 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:35:48 by mbruyant          #+#    #+#             */
-/*   Updated: 2023/12/28 18:02:59 by mbruyant         ###   ########.fr       */
+/*   Updated: 2023/12/28 18:10:03 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+/* un free sur ms->curr_work_dir causera un leak ici avec la boucle */
 static void	ft_free_prompt(t_data *ms)
 {
-	free(ms->curr_work_dir);
 	free(ms->user_input);
 	free(ms->printed_line);
 }
@@ -29,8 +29,12 @@ void	ft_loop(t_data *ms)
 		ft_printf_fd(1, "ms->user_input = %s\nms->printed_line = %s\n", \
 		ms->user_input, ms->printed_line);
 		add_history(ms->user_input);
+		if (!ft_strncmp(ms->user_input, "exit", ft_strlen(ms->user_input)))
+		{
+			ft_free_prompt(ms);
+			return ;
+		}
 		ft_free_prompt(ms);
-		return ;
 		//addhistory
 		//premiere couche parsing
 		//init struct
@@ -54,6 +58,7 @@ int	main(int argc, char **argv, char **envp)
 	ft_loop(ms);
 	ft_env_free(ms->env_struct->node_);
 	free(ms->env_struct);
+	free(ms->curr_work_dir);
 	free(ms);
 	return (0);
 }
