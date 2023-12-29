41711c2526554573448dea4700d2b1f38a9835ce
mbruyant <mbruyant@student.42.fr>
2023-12-29 18:02:36 +0100

    created ft_increment_shlvl, self explanatory, doesn't deal with the case of UNSET shlvl prior to the launch of minishell

--- init/init_env_struct.c
+++ init/init_env_struct.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:59:03 by mbruyant          #+#    #+#             */
-/*   Updated: 2023/12/28 17:31:00 by mbruyant         ###   ########.fr       */
+/*   Updated: 2023/12/29 17:55:30 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,20 @@ bool	ft_env_struct_init(t_data *ms, char **envp)
 	else
 		ms->env_struct = env_struct;
 	return (true);
+}
+
+/* IF DOESN'T FIND THE NODE (used right after init, 
+so in case of unset SHLVL before) SHOULD WE CREATE ONE ? */
+void	ft_increment_shlvl(t_env_node *envi)
+{
+	int			shlvl_value;
+	t_env_node	*shlvl_node;
+
+	shlvl_node = ft_get_node(envi, "SHLVL");
+	if (!shlvl_node)
+		return (ft_msg_end("couldn't fetch the node", 'm', false, NULL));
+	shlvl_value = ft_atoi(shlvl_node->n_content);
+	shlvl_value++;
+	free(shlvl_node->n_content);
+	shlvl_node->n_content = ft_itoa(shlvl_value);
 }
