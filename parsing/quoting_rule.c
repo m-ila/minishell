198d1307bb5db8ba36c50154c171f2446444173a
Marianne BRUYANT <mbruyant@paul-f4Br2s1.clusters.42paris.fr>
2024-01-11 14:25:05 +0100

    working on quoting rule

--- parsing/quoting_rule.c
+++ parsing/quoting_rule.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 10:20:10 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/01/11 10:34:06 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/01/11 12:47:18 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,12 @@ est-ce qu'il serait pas wise de check jusqu'a start == i ?
 int	ft_elem_is_in_quotes(char *str, int i)
 {
 	char	reigning_quote;
-	int		nb_reigning_quotes;
 	int		nb_quotes;
 	int		start;
 
 	if (!str || i >= (int) ft_strlen(str))
 		return (-1);
 	reigning_quote = '.';
-	nb_reigning_quotes = 0;
 	nb_quotes = 0;
 	start = 0;
 	while (str[start] && start < i)
@@ -46,7 +44,6 @@ int	ft_elem_is_in_quotes(char *str, int i)
 		str[start] != reigning_quote && nb_quotes % 2 == 0)
 		{
 			reigning_quote = str[start];
-			nb_reigning_quotes++;
 			nb_quotes = 0;
 		}
 		if (str[start] == reigning_quote)
@@ -54,4 +51,50 @@ int	ft_elem_is_in_quotes(char *str, int i)
 		start++;
 	}
 	return (ft_return_qu_val(reigning_quote, nb_quotes));
+}
+
+bool    ft_char_is_a_reigning_quote(char *str, int i)
+{
+    char	reigning_quote;
+	int		nb_quotes;
+	int		start;
+
+	if (!str || i >= (int) ft_strlen(str) || \
+    !ft_char_in_base(str[i], BASE_QUOTES))
+		return (false);
+	reigning_quote = '.';
+	nb_quotes = 0;
+	start = 0;
+	while (str[start] && start < i)
+	{
+		if (ft_char_in_base(str[start], BASE_QUOTES) && \
+		str[start] != reigning_quote && nb_quotes % 2 == 0)
+		{
+			reigning_quote = str[start];
+			nb_quotes = 0;
+		}
+		if (str[start] == reigning_quote)
+			nb_quotes++;
+		start++;
+	}
+    if (str[i] == reigning_quote && nb_quotes % 2 != 0)
+        return (true);
+    return (false);
+}
+
+/*
+commentaire : shouldn't it be from >= ft_strlen - 1 since
+                                        until = from + 1 ?
+to be tested later
+*/
+int ft_get_index_next_reign_quo(char *str, int from)
+{
+    int until;
+
+    if (!str || from >= (int) ft_strlen(str))
+        return (NO_INDEX_FOUND);
+    until = from + 1;
+    while (str[until] && !ft_char_is_a_reigning_quote(str, until))
+        until++;
+    return (until);
 }
