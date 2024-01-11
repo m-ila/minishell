06c4279b5e50b5508c993ea1b7d766b389886100
Marianne <bruyantmarianne@gmail.com>
2024-01-11 17:58:25 +0100

    added ft_strocc_from

--- libft/ft_str_occ.c
+++ libft/ft_str_occ.c
@@ -6,11 +6,27 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 14:13:58 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/01/01 14:20:16 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/01/11 17:55:01 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_strocc_from(char *str, char c, int from)
+{
+	int occ;
+
+	if (!str || from >= (int) ft_strlen(str))
+		return (0);
+	occ = 0;
+	while (str[from])
+	{
+		if (str[from] == c)
+			occ++;
+		from++;
+	}
+	return (occ);
+}
 
 int	ft_strocc(char *str, char c)
 {
@@ -31,41 +47,41 @@ int	ft_strocc(char *str, char c)
 /* returns the number of occurence that fits in a base */
 int ft_strocc_base(char *str, char *base)
 {
-    int i;
-    int occ;
-    int len;
+	int i;
+	int occ;
+	int len;
 
-    i = 0;
-    occ = 0;
-    len = (int) ft_strlen(str);
-    if (!str || !base)
-        return (-1);
-    while (i < len)
-    {
-        if (ft_char_in_base(str[i], base))
-            occ++;
-        i++;        
-    }
-    return (occ);
+	i = 0;
+	occ = 0;
+	len = (int) ft_strlen(str);
+	if (!str || !base)
+		return (-1);
+	while (i < len)
+	{
+		if (ft_char_in_base(str[i], base))
+			occ++;
+		i++;        
+	}
+	return (occ);
 }
 
 /* returns the number of occurence that doesn't fit in a base */
 int ft_strocc_unbase(char *str, char *base)
 {
-    int i;
-    int occ;
-    int len;
+	int i;
+	int occ;
+	int len;
 
-    i = 0;
-    occ = 0;
-    len = (int) ft_strlen(str);
-    if (!str || !base)
-        return (-1);
-    while (i < len)
-    {
-        if (!ft_char_in_base(str[i], base))
-            occ++;
-        i++;
-    }
-    return (occ);
+	i = 0;
+	occ = 0;
+	len = (int) ft_strlen(str);
+	if (!str || !base)
+		return (-1);
+	while (i < len)
+	{
+		if (!ft_char_in_base(str[i], base))
+			occ++;
+		i++;
+	}
+	return (occ);
 }
