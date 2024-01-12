0f63a83f7038607e1b862a49fcd1d10592d3e3ed
Marianne <bruyantmarianne@gmail.com>
2024-01-12 18:25:52 +0100

    [NORME OK] ft_str_occ.c

--- libft/ft_str_occ.c
+++ libft/ft_str_occ.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 14:13:58 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/01/11 17:55:01 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/01/12 18:25:26 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strocc_from(char *str, char c, int from)
 {
-	int occ;
+	int	occ;
 
 	if (!str || from >= (int) ft_strlen(str))
 		return (0);
@@ -45,11 +45,11 @@ int	ft_strocc(char *str, char c)
 }
 
 /* returns the number of occurence that fits in a base */
-int ft_strocc_base(char *str, char *base)
+int	ft_strocc_base(char *str, char *base)
 {
-	int i;
-	int occ;
-	int len;
+	int	i;
+	int	occ;
+	int	len;
 
 	i = 0;
 	occ = 0;
@@ -60,17 +60,17 @@ int ft_strocc_base(char *str, char *base)
 	{
 		if (ft_char_in_base(str[i], base))
 			occ++;
-		i++;        
+		i++;
 	}
 	return (occ);
 }
 
 /* returns the number of occurence that doesn't fit in a base */
-int ft_strocc_unbase(char *str, char *base)
+int	ft_strocc_unbase(char *str, char *base)
 {
-	int i;
-	int occ;
-	int len;
+	int	i;
+	int	occ;
+	int	len;
 
 	i = 0;
 	occ = 0;
