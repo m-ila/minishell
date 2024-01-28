c578551a6b6b8a6db707b544eb3c582e801a117d
Marianne <bruyantmarianne@gmail.com>
2024-01-28 19:14:40 +0100

    normé

--- parsing/groups_fill.c
+++ parsing/groups_fill.c
@@ -6,7 +6,7 @@
 /*   By: mbruyant <mbruyant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/28 13:51:04 by mbruyant          #+#    #+#             */
-/*   Updated: 2024/01/28 14:14:53 by mbruyant         ###   ########.fr       */
+/*   Updated: 2024/01/28 19:12:47 by mbruyant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,11 @@ bool	ft_fill_out_arr(t_data *ms, t_group *grp, t_node *from, t_node *until)
 	while (navig && i < grp->gr_nb_outfile)
 	{
 		if (i > 0 && navig->prev == until)
-			break;
+			break ;
 		if (ft_is_output_redirect(navig->tok_prv_tok))
 		{
-			tmp = ft_triple_join(navig->prev_tok, " ", navig->ep_all_elem[0], ms);
+			tmp = ft_triple_join(navig->prev_tok, " ", \
+			navig->ep_all_elem[0], ms);
 			if (!tmp || !ft_add_str_to_tab(&grp->outfile_arr, tmp))
 				return (false);
 			ft_multiple_free(&tmp, NULL, NULL);
@@ -66,10 +67,11 @@ bool	ft_fill_in_arr(t_data *ms, t_group *grp, t_node *from, t_node *until)
 	while (navig && i < grp->gr_nb_infile)
 	{
 		if (i > 0 && navig->prev == until)
-			break;
+			break ;
 		if (ft_is_input_redirect(navig->tok_prv_tok))
 		{
-			tmp = ft_triple_join(navig->prev_tok, " ", navig->ep_all_elem[0], ms);
+			tmp = ft_triple_join(navig->prev_tok, " ", \
+			navig->ep_all_elem[0], ms);
 			if (!tmp || !ft_add_str_to_tab(&grp->infile_arr, tmp))
 				return (false);
 			ft_multiple_free(&tmp, NULL, NULL);
@@ -80,7 +82,7 @@ bool	ft_fill_in_arr(t_data *ms, t_group *grp, t_node *from, t_node *until)
 	return (true);
 }
 
-bool	ft_fill_cmd_in_groups(t_data *ms, t_group *grp, t_node *from, t_node *until)
+bool	ft_fill_cmd_in_groups(t_data *ms, t_group *grp, t_node *f, t_node *u)
 {
 	size_t	i;
 	t_node	*navig;
@@ -88,10 +90,10 @@ bool	ft_fill_cmd_in_groups(t_data *ms, t_group *grp, t_node *from, t_node *until
 	if (!ms->b_temoin)
 		return (true);
 	i = 0;
-	navig = from;
+	navig = f;
 	while (navig)
 	{
-		if ((i > 0 && navig->prev == until) || i >= grp->gr_nb_elems)
+		if ((i > 0 && navig->prev == u) || i >= grp->gr_nb_elems)
 			return (true);
 		if (navig->cmd && i < grp->gr_nb_elems && !ft_special_case(navig))
 			if (!ft_add_tabs(&grp->cmd_and_args, navig->cmd_w_args))
@@ -115,10 +117,11 @@ bool	ft_fill_group_struct(t_data *ms, t_parse *p, t_node **from, t_group *g)
 	while (*from)
 	{
 		if (i > 0 && (*from)->prev == until)
-			break;
+			break ;
 		if ((*from)->tok_prv_tok == redir_in || (*from)->tok_prv_tok == heredoc)
 			g->gr_nb_infile++;
-		if ((*from)->tok_prv_tok == redir_out || (*from)->tok_prv_tok == redir_out_app)
+		if ((*from)->tok_prv_tok == redir_out || \
+		(*from)->tok_prv_tok == redir_out_app)
 			g->gr_nb_outfile++;
 		g->gr_nb_elems++;
 		(*from) = (*from)->next;
